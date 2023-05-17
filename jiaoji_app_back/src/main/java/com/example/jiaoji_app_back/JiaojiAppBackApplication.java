0c6fd1f6e5c83637278c78fb2c655241430e3f57
Quan <qyl728@sjtu.edu.cn>
2023-05-17 16:52:34 +0800

    update

--- jiaoji_app_back/src/main/java/com/example/jiaoji_app_back/JiaojiAppBackApplication.java
+++ jiaoji_app_back/src/main/java/com/example/jiaoji_app_back/JiaojiAppBackApplication.java
@@ -2,6 +2,7 @@ package com.example.jiaoji_app_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class JiaojiAppBackApplication {
